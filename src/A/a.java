181b70697fa04005157a8485bededa729c01391f
admin <admin@DESKTOP-D0L5MJJ>
2022-08-02 15:30:57 +0530

    Added new code

---
+++ src/A/a.java
@@ -0,0 +1,9 @@
+package A;
+
+public class a {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+}
---
+++ src/A/b.java
@@ -0,0 +1,8 @@
+package A;
+
+public class b {
+public static void main(String[] args) {
+		
+		System.out.println("Hi");
+	}
+}
